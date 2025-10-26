beaf5d15e21f5ef2e7a852afa3acd77cbd7a4177
hydra4004 <syedahmedfarrukh7@gmail.com>
2025-10-26 21:06:28 +0500

    Modify proc struct to include usyscall
    Added usyscall field to proc struct and removed mask and allowed_path.

--- kernel/proc.h
+++ kernel/proc.h
@@ -1,4 +1,3 @@
-#define MAXPATH 128
 // Saved registers for kernel context switches.
 struct context {
   uint64 ra;
@@ -85,6 +84,7 @@ enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 // Per-process state
 struct proc {
   struct spinlock lock;
+  struct usyscall *usyscall;
 
   // p->lock must be held when using these:
   enum procstate state;        // Process state
@@ -99,13 +99,10 @@ struct proc {
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
   uint64 sz;                   // Size of process memory (bytes)
-  uint64 mask;
-  char allowed_path[MAXPATH];
   pagetable_t pagetable;       // User page table
   struct trapframe *trapframe; // data page for trampoline.S
   struct context context;      // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-
 };
